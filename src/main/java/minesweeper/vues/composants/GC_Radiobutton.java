f825876659aed499e32ec36ad1d3bec6579344a8
Maxime Vannérum <maxime.vannerum@hotmail.com>
2020-02-10 10:51:51 -0500

    Refactorisation de parametres.xml afin d'utiliser les composants GC_Radiobutton

--- src/main/java/minesweeper/vues/composants/GC_Radiobutton.java
+++ src/main/java/minesweeper/vues/composants/GC_Radiobutton.java
@@ -13,58 +13,61 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
 
 public class GC_Radiobutton extends VBox {
-			
-		public GC_Radiobutton(@NamedArg("rb1") String rb1_Name,
-								@NamedArg("rb2") String rb2_Name,
-								@NamedArg("rb3") String rb3_Name,
-								@NamedArg("title") String title) {
-			
-			super();
-			J.appel( this );
-			ToggleGroup tg = new ToggleGroup();
-			this.getChildren().add( AjouterVBoxTitre( title ) );
-			this.getChildren().add( AjouterBouton( rb1_Name, tg ) );
-			this.getChildren().add( AjouterVBoxVide());
-			this.getChildren().add( AjouterBouton( rb2_Name, tg ) );
-			this.getChildren().add( AjouterVBoxVide());
-			this.getChildren().add( AjouterBouton( rb3_Name, tg ) );
-			this.getChildren().add( AjouterVBoxVide());
-			
-		}
-		
-		private RadioButton AjouterBouton(String texte, ToggleGroup tg) {
-			RadioButton rb = new RadioButton(texte);
-			rb.setToggleGroup( tg );
-			rb.setMaxHeight( Double.MAX_VALUE );
-			VBox.setVgrow( rb, Priority.ALWAYS );
-			return rb;
-		}
-		
-		private VBox AjouterVBoxVide() {
-			VBox vb = new VBox();
-			
-			vb.setMaxHeight( Double.MAX_VALUE );
-			VBox.setVgrow( vb, Priority.ALWAYS );
-			
-			return vb;
-		}
-		
-		private VBox AjouterVBoxTitre(String texte) {
-			VBox vb = new VBox();
-			
-			vb.setMaxHeight( Double.MAX_VALUE );
-			VBox.setVgrow( vb, Priority.ALWAYS );
-			
-			Text text = new Text(texte);
-			text.getStyleClass().add( "titreToggleGroup" );
-			
-			vb.getChildren().add( text );
-			
-			return vb;
-			
-		}
-		
-		
+
+	public GC_Radiobutton( @NamedArg("rb1") String rb1_Name, @NamedArg("rb2") String rb2_Name,
+			@NamedArg("rb3") String rb3_Name, @NamedArg("title") String title ) {
+
+		super();
+		J.appel( this );
+		ToggleGroup tg = new ToggleGroup();
+
+		this.getChildren().add( AjouterVBoxTitre( title ) );
+		this.getChildren().add( AjouterBouton( rb1_Name, tg ) );
+		this.getChildren().add( AjouterVBoxVide() );
+		this.getChildren().add( AjouterBouton( rb2_Name, tg ) );
+		this.getChildren().add( AjouterVBoxVide() );
+		this.getChildren().add( AjouterBouton( rb3_Name, tg ) );
+		this.getChildren().add( AjouterVBoxVide() );
+
 	}
 
+	private RadioButton AjouterBouton( String texte, ToggleGroup tg ) {
+		J.appel( this );
 
+		RadioButton rb = new RadioButton( texte );
+		rb.setToggleGroup( tg );
+		rb.setMaxHeight( Double.MAX_VALUE );
+		VBox.setVgrow( rb, Priority.ALWAYS );
+
+		return rb;
+	}
+
+	private VBox AjouterVBoxVide() {
+		J.appel( this );
+
+		VBox vb = new VBox();
+
+		vb.setMaxHeight( Double.MAX_VALUE );
+		VBox.setVgrow( vb, Priority.ALWAYS );
+
+		return vb;
+	}
+
+	private VBox AjouterVBoxTitre( String texte ) {
+		J.appel( this );
+
+		VBox vb = new VBox();
+
+		vb.setMaxHeight( Double.MAX_VALUE );
+		VBox.setVgrow( vb, Priority.ALWAYS );
+
+		Text text = new Text( texte );
+		text.getStyleClass().add( "titreToggleGroup" );
+
+		vb.getChildren().add( text );
+
+		return vb;
+
+	}
+
+}
