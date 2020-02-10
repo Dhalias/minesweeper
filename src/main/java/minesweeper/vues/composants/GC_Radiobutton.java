78f5c83c702e4e93097e8eb3ee65696f894fb906
Maxime Vannérum <maxime.vannerum@hotmail.com>
2020-02-10 10:32:43 -0500

    Ajout du composant GC_Radiobutton
    GC -> GroupControl

---
+++ src/main/java/minesweeper/vues/composants/GC_Radiobutton.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package minesweeper.vues.composants;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import commun.debogage.J;
+import javafx.beans.NamedArg;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
+
+public class GC_Radiobutton extends VBox {
+			
+		public GC_Radiobutton(@NamedArg("rb1") String rb1_Name,
+								@NamedArg("rb2") String rb2_Name,
+								@NamedArg("rb3") String rb3_Name,
+								@NamedArg("title") String title) {
+			
+			super();
+			J.appel( this );
+			ToggleGroup tg = new ToggleGroup();
+			this.getChildren().add( AjouterVBoxTitre( title ) );
+			this.getChildren().add( AjouterBouton( rb1_Name, tg ) );
+			this.getChildren().add( AjouterVBoxVide());
+			this.getChildren().add( AjouterBouton( rb2_Name, tg ) );
+			this.getChildren().add( AjouterVBoxVide());
+			this.getChildren().add( AjouterBouton( rb3_Name, tg ) );
+			this.getChildren().add( AjouterVBoxVide());
+			
+		}
+		
+		private RadioButton AjouterBouton(String texte, ToggleGroup tg) {
+			RadioButton rb = new RadioButton(texte);
+			rb.setToggleGroup( tg );
+			rb.setMaxHeight( Double.MAX_VALUE );
+			VBox.setVgrow( rb, Priority.ALWAYS );
+			return rb;
+		}
+		
+		private VBox AjouterVBoxVide() {
+			VBox vb = new VBox();
+			
+			vb.setMaxHeight( Double.MAX_VALUE );
+			VBox.setVgrow( vb, Priority.ALWAYS );
+			
+			return vb;
+		}
+		
+		private VBox AjouterVBoxTitre(String texte) {
+			VBox vb = new VBox();
+			
+			vb.setMaxHeight( Double.MAX_VALUE );
+			VBox.setVgrow( vb, Priority.ALWAYS );
+			
+			Text text = new Text(texte);
+			text.getStyleClass().add( "titreToggleGroup" );
+			
+			vb.getChildren().add( text );
+			
+			return vb;
+			
+		}
+		
+		
+	}
+
+
