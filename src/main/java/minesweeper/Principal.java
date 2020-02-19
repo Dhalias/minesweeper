f1e111259fe1715cf92fc45f1b9a0f96b593530d
Maxime Vannérum <maxime.vannerum@hotmail.com>
2020-02-19 17:22:46 -0500

    Instanciation du MVC

--- src/main/java/minesweeper/Principal.java
+++ src/main/java/minesweeper/Principal.java
@@ -1,12 +1,19 @@
 package minesweeper;
 
+import javax.naming.PartialResultException;
+
 import commun.debogage.DoitEtre;
 import commun.debogage.J;
+import commun_client.mvc.controleurs.FabriqueControleur;
 import commun_javafx.ChargeurDeVue;
 import commun_javafx.Initialisateur;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import minesweeper.modeles.Parametres.Parametres;
+import minesweeper_javafx.afficheurs.AfficheurParametresFX;
+import minesweeper_javafx.controleurs.ControleurParametresFX;
+import minesweeper_javafx.vues.VueParametresFX;
 
 public class Principal extends Application{
 	static {
@@ -34,7 +41,19 @@ public class Principal extends Application{
 	private Scene creerScenePrincipale() {
 		J.appel( this );
 		
-		ChargeurDeVue chargeur = new ChargeurDeVue("/fxml/parametres.xml","traductions.chaines","/css/parametres.css");
+		ChargeurDeVue<VueParametresFX> chargeur; 
+		chargeur = new ChargeurDeVue<VueParametresFX>("/fxml/parametres.xml","traductions.chaines","/css/parametres.css");
+		
+		VueParametresFX vue = chargeur.getVue();
+		
+		DoitEtre.nonNul( vue );
+		
+		Parametres parametres = new Parametres();
+		
+		AfficheurParametresFX afficheur = new AfficheurParametresFX();
+		
+		FabriqueControleur.creerControleur( ControleurParametresFX.class, parametres, vue, afficheur );
+		
 		Scene scene = chargeur.nouvelleScene((float)25,(float)37.03,(float)1.50);
 		
 		DoitEtre.nonNul( scene );
--- src/main/java/minesweeper_javafx/vues/VueParametresFX.java
+++ src/main/java/minesweeper_javafx/vues/VueParametresFX.java
@@ -5,14 +5,21 @@ import minesweeper_client.vues.VueParametres;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import commun.debogage.DoitEtre;
 import commun.debogage.J;
 import javafx.fxml.*;
+import minesweeper.vues.composants.GC_Radiobutton;
 
 public class VueParametresFX implements VueParametres, Initializable {
 
+	@FXML
+	private GC_Radiobutton gcRadiobutton;
+	
 	@Override
 	public void initialize( URL arg0, ResourceBundle arg1 ) {
 		J.appel( this );
+		
+		DoitEtre.nonNul( gcRadiobutton );
 		
 	}
 
