b3a920d37becf10f0a139b8f6febae4785731f5f
Maxime Vannérum <maxime.vannerum@hotmail.com>
2020-02-26 13:35:02 -0500

    erreur04
    [Erreur] À la ligne 28, il est impossible de charger l'élément Button,
    
    J# (ChargeurDeVue.java:153) FATAL: impossible de charger le parent
    javafx.fxml.LoadException:
    /C:/Users/Maxim/OneDrive/C%c3%a9gep/1%20-%20Eclipse_WorkspacePhoton/Minesweeper/build/resources/main/fxml/parametres.xml:28

--- src/main/java/minesweeper_javafx/vues/VueParametresFX.java
+++ src/main/java/minesweeper_javafx/vues/VueParametresFX.java
@@ -1,7 +1,10 @@
 package minesweeper_javafx.vues;
 
+import minesweeper_client.commandes.FaireDebutPartie;
+import minesweeper_client.commandes.FaireDebutPartiePourEnvoi;
 import minesweeper_client.vues.VueParametres;
 
+import java.awt.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,6 +12,7 @@ import commun.debogage.DoitEtre;
 import commun.debogage.J;
 import javafx.fxml.*;
 import javafx.scene.layout.*;
+import minesweeper.modeles.Parametres.Parametres;
 import minesweeper.vues.composants.GC_Radiobutton;
 
 public class VueParametresFX implements VueParametres, Initializable {
@@ -19,6 +23,9 @@ public class VueParametresFX implements VueParametres, Initializable {
 	@FXML
 	private VBox root;
 	
+	@FXML
+	private Button boutonDebuter;
+	
 	@Override
 	public void initialize( URL arg0, ResourceBundle arg1 ) {
 		J.appel( this );
@@ -27,9 +34,49 @@ public class VueParametresFX implements VueParametres, Initializable {
 		
 	}
 	
-	public void changerCouleurArrierrePlan(String couleur) {
+	public void changerCouleurArrierrePlan() {
 		J.appel( this );
-		root.getStyleClass().add( couleur );
+		root.getStyleClass().add( calculerCouleur() );
+	}
+	
+	private String calculerCouleur() {
+		J.appel( this );
+		String couleur = "grey";
+		switch ( (int) (Math.random()*4) ) {
+		case 0:
+			couleur = "coral";
+			break;
+		case 1:
+			couleur = "green";
+			break;
+		
+		case 2:
+			couleur = "darkRed";
+			break;
+			
+		case 3:
+			couleur = "blue";
+			break;
+		}
+		return couleur;
+	}
+	
+	
+
+	@Override
+	public void obtenirCommandesPourEnvoi() {
+		J.appel( this );
+		
+		
+	}
+	
+	public void obtenirFaireDebutPartiePourEnvoi() {
+		
+	}
+
+	@Override
+	public void installerCapteursEvenementsUsager() {
+		
 	}
 
 
