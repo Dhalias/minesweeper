669d11e022c1e2bbd692fed44df826f77944c434
Maxime Vannérum <maxime.vannerum@hotmail.com>
2020-03-03 16:16:33 -0500

    Ajout de la commande OuvrirParametres

---
+++ src/main/java/minesweeper_client/commandes/OuvrirParametres.java
@@ -0,0 +1,8 @@
+package minesweeper_client.commandes;
+
+import commun_client.commandes.Commande;
+
+public class OuvrirParametres extends Commande<OuvrirParametresPourEnvoi, OuvrirParametresRecue>
+implements OuvrirParametresPourEnvoi, OuvrirParametresRecue {
+
+}
---
+++ src/main/java/minesweeper_client/commandes/OuvrirParametresRecue.java
@@ -0,0 +1,7 @@
+package minesweeper_client.commandes;
+
+import commun_client.commandes.CommandeRecue;
+
+public interface OuvrirParametresRecue extends CommandeRecue{
+
+}
