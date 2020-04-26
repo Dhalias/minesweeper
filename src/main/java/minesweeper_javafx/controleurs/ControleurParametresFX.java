7223167a89f4332a3009f349542208ec179cf64e
Maxime Vannérum <maxime.vannerum@hotmail.com>
2020-04-26 14:58:40 -0400

    Ajout de la méthode ouvrirPartie dans le controleurParametres

--- src/main/java/minesweeper_javafx/controleurs/ControleurParametresFX.java
+++ src/main/java/minesweeper_javafx/controleurs/ControleurParametresFX.java
@@ -1,8 +1,14 @@
 package minesweeper_javafx.controleurs;
 
+import commun.debogage.DoitEtre;
 import commun.debogage.J;
 import commun.systeme.Systeme;
+import commun_client.mvc.controleurs.FabriqueControleur;
 import commun_client.mvc.controleurs.RecepteurCommandeMVC;
+import commun_javafx.ChargeurDeVue;
+import commun_javafx.DialogueModal;
+import javafx.scene.Scene;
+import minesweeper.modeles.Parametres.Parametres;
 import minesweeper_client.commandes.FaireDebutPartie;
 import minesweeper_client.commandes.FaireDebutPartieRecue;
 import minesweeper_client.commandes.OuvrirParametres;
@@ -30,11 +36,37 @@ public class ControleurParametresFX extends ControleurParametres<VueParametresFX
 			@Override
 			public void executerCommandeMVC( FaireDebutPartieRecue commande ) {
 				J.appel( this );
-				
+				ouvrirPartie();
 			}
 	    });
 	      
 	    
+	}
+
+	private void ouvrirPartie() {
+		J.appel( this );
+		
+		final String CHEMIN_PARTIELOCALE_FXML = "/fxml/partieLocale.xml";
+	    final String CHEMIN_CHAINES = "traductions.chaines";
+	    final String CHEMIN_PARTIE_CSS = "/css/partieLocale.css";
+
+	    ChargeurDeVue<VuePartieLocaleFX> chargeur; 
+		chargeur = new ChargeurDeVue<VuePartieLocaleFX>(CHEMIN_PARTIELOCALE_FXML,CHEMIN_CHAINES,CHEMIN_PARTIE_CSS);
+		
+		VuePartieLocaleFX vue = chargeur.getVue();
+		
+		DoitEtre.nonNul( vue );
+		
+		PartieLocale partieLocale = new PartieLocale();
+		
+		AfficheurPartieLocaleFX afficheur = new AfficheurPartieLocaleFX();
+		
+		FabriqueControleur.creerControleur( ControleurPartieLocaleFX.class, partieLocale, vue, afficheur );
+		
+		Scene scene = chargeur.nouvelleScene((float)25,(float)37.03,(float)1.50);
+	    
+	   DialogueModal.ouvrirDialogueModal(scene);
+		
 	}
 
 }
