239730daa9fe2f9da80232a40d0998115a8be9b8
Maxime Vannérum <maxime.vannerum@hotmail.com>
2020-04-26 14:47:37 -0400

    Ajout du recepteur FaireDebutPartie dans ControleurParametres

--- src/main/java/minesweeper_javafx/controleurs/ControleurParametresFX.java
+++ src/main/java/minesweeper_javafx/controleurs/ControleurParametresFX.java
@@ -1,6 +1,14 @@
 package minesweeper_javafx.controleurs;
 
 import commun.debogage.J;
+import commun.systeme.Systeme;
+import commun_client.mvc.controleurs.RecepteurCommandeMVC;
+import minesweeper_client.commandes.FaireDebutPartie;
+import minesweeper_client.commandes.FaireDebutPartieRecue;
+import minesweeper_client.commandes.OuvrirParametres;
+import minesweeper_client.commandes.OuvrirParametresRecue;
+import minesweeper_client.commandes.Quitter;
+import minesweeper_client.commandes.QuitterRecue;
 import minesweeper_client.controleurs.ControleurParametres;
 import minesweeper_javafx.afficheurs.AfficheurParametresFX;
 import minesweeper_javafx.vues.VueParametresFX;
@@ -11,6 +19,22 @@ public class ControleurParametresFX extends ControleurParametres<VueParametresFX
 	public void demarrer() {
 		J.appel( this );
 		
+	}
+	
+	@Override
+	public void installerReceptionCommandes() {
+	    J.appel(this);
+
+	    installerRecepteurCommande(FaireDebutPartie.class, new RecepteurCommandeMVC<FaireDebutPartieRecue>() {
+
+			@Override
+			public void executerCommandeMVC( FaireDebutPartieRecue commande ) {
+				J.appel( this );
+				
+			}
+	    });
+	      
+	    
 	}
 
 }
